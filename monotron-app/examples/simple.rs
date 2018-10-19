c83edd0a9df161f7806524ff59497e4c8c3035ae
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-10-19 15:46:13 -0400

    Added a few more APIs.

--- monotron-app/examples/simple.rs
+++ monotron-app/examples/simple.rs
@@ -2,9 +2,17 @@
 #![no_main]
 
 extern crate monotron_app;
+use monotron_app::prelude::*;
+use monotron_app::Host;
 
 #[no_mangle]
 pub extern "C" fn main() -> i32 {
-	// println!("Hello, Rust!");
-	123
+	// Print 5 times in 5 seconds
+	for _ in 0..5 {
+	    write!(Host, "Hello, Rust!\n").unwrap();
+		for _ in 0..60 {
+			Host::wfvbi();
+		}
+	}
+    0
 }
--- monotron-app/src/lib.rs
+++ monotron-app/src/lib.rs
@@ -14,35 +14,96 @@
 
 #![no_std]
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::sync::atomic::{self, Ordering};
 
-#[link_section = ".entry_point"]
-pub static ENTRY_POINT: fn(*const Context) -> i32 = entry_point;
-
 #[repr(C)]
-pub struct Context {
-	p_context: *const u8
+pub struct Table {
+    p_context: *const Context,
+    putchar: extern "C" fn(*const Context, u8) -> i32,
+    puts: extern "C" fn(*const Context, *const u8) -> i32,
+    readc: extern "C" fn(*const Context) -> i32,
+    wfvbi: extern "C" fn(*const Context),
+    kbhit: extern "C" fn(*const Context) -> i32,
+    move_cursor: extern "C" fn(*const Context, u8, u8),
+    play: extern "C" fn (*const Context, u32, u8, u8, u8) -> i32,
+    change_font: extern "C" fn (*const Context, u32, *const u8),
+    get_joystick: extern "C" fn (*const Context) -> u8
 }
 
-#[no_mangle]
-pub fn entry_point(_raw_ctx: *const Context) -> i32 {
-	// Do stuff with _raw_ctx
+#[link_section = ".entry_point"]
+pub static ENTRY_POINT: fn(*const Table) -> i32 = entry_point;
+pub struct Host;
 
-	// call the user application
-	extern "C" {
-		fn main() -> i32;
+struct Context;
+
+static mut TABLE_POINTER: Option<&'static Table> = None;
+
+#[no_mangle]
+pub fn entry_point(raw_ctx: *const Table) -> i32 {
+    // Turn the pointer into a reference and store in a static.
+    unsafe {
+        let ctx = &*raw_ctx;
+        TABLE_POINTER = Some(ctx);
+    };
+
+    extern "C" {
+        fn main() -> i32;
+    }
+    // call the user application
+    unsafe { main() }
+}
+
+impl Table {
+    fn get() -> &'static Table {
+        unsafe {
+            if let Some(tbl) = &TABLE_POINTER {
+                tbl
+            } else {
+                panic!("Bad context");
+            }
+        }
+    }
+}
+
+impl core::fmt::Write for Host {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        let tbl = Table::get();
+        for b in s.bytes() {
+            (tbl.putchar)(tbl.p_context, b);
+        }
+        Ok(())
+    }
+}
+
+impl Host {
+	pub fn kbhit() -> bool {
+	    let tbl = Table::get();
+		(tbl.kbhit)(tbl.p_context) != 0
 	}
-	unsafe {
-		main()
+
+	pub fn readc() -> i32 {
+	    let tbl = Table::get();
+		(tbl.readc)(tbl.p_context)
+	}
+
+	pub fn wfvbi() {
+	    let tbl = Table::get();
+		(tbl.wfvbi)(tbl.p_context)
 	}
 }
 
 #[inline(never)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-	// eprintln!("Panic: {:?}", _info);
+    // This uses a *LOT* of our 24 KiB of RAM
+    // write!(Host, "\u{001B}Z\u{001B}R\u{001B}kPanic: {:?}\u{001B}W", _info);
     loop {
         atomic::compiler_fence(Ordering::SeqCst);
     }
+}
+
+pub mod prelude {
+    pub use core::fmt::Write as _monotron_prelude_core_fmt_Write;
 }
