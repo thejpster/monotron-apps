b0d4d0c3c6bec599a98de787928f2500aa92f7b3
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-04-19 19:10:11 +0100

    Remove debug

--- monotron-slideshow/compress.py
+++ monotron-slideshow/compress.py
@@ -6,16 +6,11 @@ f_out = open(sys.argv[2], "wb")
 last = None
 last_count = 1
 for b in f_in.read():
-    print("Got byte 0x{:02x}".format(b))
     if b == last:
-        print("Dup!")
         last_count += 1
     else:
-        if last:
-            print("Diff - dumping {} of 0x{:02x}".format(last_count, last))
         if last_count > 4:
             f_out.write(bytes([ord('^'), ord('n'), 32 + last_count, last]))
-            # f_out.write("^n{}{}".format(chr(32 + last_count), chr(last)).encode("cp850"))
         elif last is not None:
             f_out.write(bytes([last] * last_count))
         last = b
